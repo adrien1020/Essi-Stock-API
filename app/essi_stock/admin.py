9e389d76e4b739650a5f0941bcc96ea2ab182253
Adrien <62828317+adrien1020@users.noreply.github.com>
2022-05-31 06:17:35 +0200

    Added list_display to admin categorites

--- app/essi_stock/admin.py
+++ app/essi_stock/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from .models import CategoriteLevelTwo, Item, CategoriteLevelOne, Categorite
 
 
-class CategoryAdmin(admin.ModelAdmin):
+class CategoriteAdmin(admin.ModelAdmin):
+    list_display = ['get_categorite_name']
+
+    def get_categorite_name(self, obj):
+        return obj.name
+    get_categorite_name.short_description = 'name'
+
+
+class CategoriteLevelOneAdmin(admin.ModelAdmin):
     list_display = ['get_categorites_name']
 
     def get_categorites_name(self, obj):
@@ -10,23 +18,32 @@ class CategoryAdmin(admin.ModelAdmin):
     get_categorites_name.short_description = 'name'
 
 
+class CategoriteLevelTwoAdmin(admin.ModelAdmin):
+    list_display = ['get_categorites_name']
+
+    def get_categorites_name(self, obj):
+        return obj.name
+
+    get_categorites_name.short_description = 'name'
+
+
 class ItemAdmin(admin.ModelAdmin):
     list_display = ['get_name', 'get_level_1', 'get_level_2', 'marque', 'reference', 'price']
 
     def get_name(self, obj):
-        return obj.category.name
-    get_name.short_description = 'category Name'
+        return obj.categorite.name
+    get_name.short_description = 'categorites'
 
     def get_level_1(self, obj):
-        return obj.category_level_1.name
-    get_level_1.short_description = 'category L1'
+        return obj.categorite_level_1.name
+    get_level_1.short_description = 'categorites level One'
 
     def get_level_2(self, obj):
-        return obj.category_level_2.name
-    get_level_2.short_description = 'category L2'
+        return obj.categorite_level_2.name
+    get_level_2.short_description = 'categorites level two'
 
 
-admin.site.register(CategoriteLevelOne)
-admin.site.register(CategoriteLevelTwo)
+admin.site.register(CategoriteLevelOne, CategoriteLevelOneAdmin)
+admin.site.register(CategoriteLevelTwo, CategoriteLevelTwoAdmin)
 admin.site.register(Item, ItemAdmin)
-admin.site.register(Categorite, CategoryAdmin)
+admin.site.register(Categorite, CategoriteAdmin)
